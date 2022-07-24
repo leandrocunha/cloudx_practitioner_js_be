f3126656afce678ffaee2a3bd1e473071de55b92
Leandro Cunha <leandroscunha@gmail.com>
2022-07-24 21:54:38 +0200

    feat: add lambdas do list products, get by id and create products

---
+++ migrations/initial_stocks.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.stocks (product_id, count)
+	VALUES ('cf0b5dc7-16d7-422e-af6b-7e5484830a79', 10);
+INSERT INTO public.stocks (product_id, count)
+	VALUES ('ac42c2f9-6b20-4015-8180-649e61920bcb', 20);
+INSERT INTO public.stocks (product_id, count)
+	VALUES ('99de2b19-fcbf-4397-b1cb-d5c78c6665c7', 30);
