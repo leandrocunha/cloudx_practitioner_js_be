7242a163eaf60b71db792c0932d5c264c763b978
Leandro Cunha <leandroscunha@gmail.com>
2022-08-10 21:44:31 +0200

    fix: importProducts

--- src/import-service/importFileParser/index.js
+++ src/import-service/importFileParser/index.js
@@ -2,29 +2,22 @@
 
 exports.handler = async (event) => {
     const {GetObjectCommand, S3Client} = require('@aws-sdk/client-s3');
-    try {
-        const chunks = [];
-        const streamToString = (stream) =>
-        new Promise((resolve, reject) => {          
-          stream.on("data", (chunk) => {
-            console.log(chunk);
-            chunks.push(chunk)
-          });
-          stream.on("error", error => reject(console.log(error)));
-          stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-        });
-        
+    const csv = require('csv-parser');
+    try {        
+        const results = [];
         const {Records} = event;
         const recordsObj = Records[0];
         const {s3: {bucket: {name}, object: {key}}} = recordsObj;
         const params = {Bucket: name, Key: key};
-        console.log(params)
         const s3Client = new S3Client({ region: "eu-central-1" });
         const command = new GetObjectCommand(params);
         const data = await s3Client.send(command);
-        console.log(data);
-        const bodyContents = await streamToString(data.Body);
-        console.log(bodyContents);
+        data.Body
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                console.log(results);
+            });
       } catch (err) {
         console.log("Error", err);
       }
