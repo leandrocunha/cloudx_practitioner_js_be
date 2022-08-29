e96ce51a5d56d21082df9eb4d7dcdb512be92ef3
Leandro Cunha <leandro_cunha@epam.com>
2022-08-29 19:19:36 +0200

    feat: send message to SQS

--- src/import-service/importFileParser/index.js
+++ src/import-service/importFileParser/index.js
@@ -2,23 +2,35 @@
 
 exports.handler = async (event) => {
     const {GetObjectCommand, S3Client} = require('@aws-sdk/client-s3');
+    const {SQSClient, SendMessageCommand} = require("@aws-sdk/client-sqs");
     const csv = require('csv-parser');
-    try {        
-        const results = [];
-        const {Records} = event;
-        const recordsObj = Records[0];
-        const {s3: {bucket: {name}, object: {key}}} = recordsObj;
-        const params = {Bucket: name, Key: key};
-        const s3Client = new S3Client({ region: "eu-central-1" });
-        const command = new GetObjectCommand(params);
-        const data = await s3Client.send(command);
+
+    const results = [];
+    const {Records} = event;
+    const recordsObj = Records[0];
+    const {s3: {bucket: {name}, object: {key}}} = recordsObj;
+    const params = {Bucket: name, Key: key};
+
+    const s3Client = new S3Client({region: "eu-central-1"});
+    const command = new GetObjectCommand(params);
+    const sqsClient = new SQSClient({region: "eu-central-1"});
+    const data = await s3Client.send(command);
+
+    try {
         data.Body
             .pipe(csv())
             .on('data', (data) => results.push(data))
-            .on('end', () => {
-                console.log(results);
+            .on('end', async () => {
+                const params = {
+                    MessageBody: JSON.stringify(results),
+                    QueueUrl: process.env.SQS_URL
+                }
+                const command = new SendMessageCommand(params);
+                console.log(command);
+                const result = await sqsClient.send(command);
+                console.log('result', result);
             });
-      } catch (err) {
+    } catch (err) {
         console.log("Error", err);
-      }
+    }
 }
